cb5d8adff5fc8d3191b600d195d8c66c9594155b
William <informatica01@COOPER.local>
2019-05-06 11:08:58 -0300

    Array Básico

---
+++ array.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+
+	<?php
+
+		$objetos=array('Xbox One', 'PlayStation 4', 'Xiami MyBand');
+		$frutas=array(
+			'a'=>'Morango',
+			'b'=>'Maçã',
+			'x'=>'Abacate'
+		);
+
+		echo "<pre>";
+			var_dump($frutas);
+		echo "</pre>";
+
+	?>
+
+</body>
+</html>
